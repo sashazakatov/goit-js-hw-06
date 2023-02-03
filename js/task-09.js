fcf5be32f4552af23f475a829ee19ccb37a7496d
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-02-03 23:30:20 +0200

    Adds solutions to the tenth task

--- js/task-09.js
+++ js/task-09.js
@@ -2,11 +2,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const buttonChangeColor = document.querySelector('.change-color');
-const spanColor = document.querySelector('.color');
+const buttonChangeColorRef = document.querySelector('.change-color');
+const spanColorRef = document.querySelector('.color');
 
-buttonChangeColor.addEventListener('click', () => {
+buttonChangeColorRef.addEventListener('click', () => {
   const randomColor = getRandomHexColor();
-  spanColor.textContent = randomColor;
+  
+  spanColorRef.textContent = randomColor;
   document.body.style.backgroundColor = randomColor;
 });
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const refs = {
+  box: document.querySelector('#boxes'),
+  buttonCreate: document.querySelector('#controls > [data-create]'),
+  buttonDestroy: document.querySelector('#controls > [data-destroy]'),
+  input: document.querySelector('#controls > input'),
+};
+
+const createBoxes = (amount) =>
+{
+  const boxes = [];
+  for(let i = 0; i < amount; i++){
+    const box = document.createElement('div');
+    
+    box.style.backgroundColor = getRandomHexColor();
+    console.log(30 + (i * 10));
+    box.style.width = `${30 + (i * 10)}px`;
+    box.style.height = `${30 + (i * 10)}px`;
+
+    boxes.push(box);
+    // console.log(box.style);
+  }
+
+  refs.box.append(...boxes);
+  // console.log(refs.box);
+};
+// console.dir(refs.input);
+refs.buttonCreate.addEventListener('click', () => createBoxes(refs.input.value));
+refs.buttonDestroy.addEventListener('click', () => {
+  refs.box.innerHTML = '';
+  refs.input.value = '';
+});
