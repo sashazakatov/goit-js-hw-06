dfd956b5005fd75ca0024997fea4ada138d53aa9
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-02-03 22:07:56 +0200

    Adds a solution to the fifth task

--- js/task-09.js
+++ js/task-09.js
@@ -2,3 +2,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const buttonChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  spanColor.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
