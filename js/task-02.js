f417919de86022ca6803863de6d988233102fcdf
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-02-03 11:17:50 +0200

    adds solutions to the first and second tasks

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+const ingredientsArryLi = [];
+
+ingredients.forEach(element => {
+  const newLiRef = document.createElement('li');
+  newLiRef.textContent = element;
+  newLiRef.classList.add('item');
+  ingredientsArryLi.push(newLiRef);
+});
+
+ingredientsRef.append(...ingredientsArryLi);
